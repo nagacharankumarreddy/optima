a6554b7de6f28a6f30bc2c32a6d82a4ee929d76a
nagacharankumarreddy <nagacharankumarreddy.com>
2021-02-02 10:30:59 +0530

    Template done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,61 @@
-
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import BellIcon from "react-bell-icon";
+import { IoIosContact } from "react-icons/io";
+import { AiOutlineHome } from "react-icons/ai";
+import { BiWalletAlt } from "react-icons/bi";
+import { RiMoneyDollarCircleLine } from "react-icons/ri";
+import { GoGraph } from "react-icons/go";
+import { GiPiggyBank } from "react-icons/gi";
+import { CgMail } from "react-icons/cg";
+import { GrLogout } from "react-icons/gr";
+import Financials from "./Components/Financials.jsx";
 
 function App() {
   return (
-    <div className="Header">
-    <span id="main-head">optima</span>
-    <span id="sub">Pay Less,Save More</span>
-   
+    <div>
+      <div className="Header">
+        <div className="header-left">
+          <span id="main-head">optima</span>
+          <span id="sub">Pay Less,Save More</span>
+          <span id="bell">
+            <BellIcon width="25" active={true} animate={false} />
+          </span>
+        </div>
+        <div className="header-right">
+          <IoIosContact className="contact" />
+          <GrLogout className="logout" />
+        </div>
+      </div>
+      <div>
+        <div className="menu">
+          <a href="/home" class="active">
+            <AiOutlineHome />
+          </a>
+          <a href="/wallet">
+            <BiWalletAlt />
+          </a>
+          <a href="/">
+            <RiMoneyDollarCircleLine />
+          </a>
+          <a href="/">
+            <GoGraph />
+          </a>
+          <a href="/">
+            <GiPiggyBank />
+          </a>
+          <a href="/">
+            <CgMail />
+          </a>
+        </div>
+      </div>
+      <div>
+        <div>
+          <Router>
+            <Route path="/home" component={Financials} />
+          </Router>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Financials.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+class Financials extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="outer">
+        <div className="inner-left">
+          <h1>My Financials</h1>
+        </div>
+        <div className="inner-right">
+            <h1>carousel</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Financials;
