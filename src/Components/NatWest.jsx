fbd82cfd8d5c0fce6e24d8ceea1a46f081b35af2
Vinod Kumar Tirudhandam <vinodkumartirudhandam@c07dn022pjj9.home>
2021-02-14 15:39:35 +0000

    Updated API fetch call with json server for NatWest.jsx

--- src/Components/NatWest.jsx
+++ src/Components/NatWest.jsx
@@ -2,41 +2,64 @@ import React, { Component } from "react";
 import creditdata from "../credit.json";
 
 class Natwest extends Component {
-  state = {};
+
+  constructor(props) {
+    super(props);
+    this.state = { data: []};
+    this.getCreditData = this.getCreditData.bind(this);
+  }
+
+  async componentDidMount(){
+    var getVal = await this.getCreditData();
+    this.setState({ data: getVal });
+    console.log(this.state);
+  }
+
+  async getCreditData() {
+    const response = await fetch('http://localhost:8000/credit');
+    const json = await response.json();
+    return json;
+  }
+
   render() {
-    return (
-      <div className="hover-card-main">
-        <div>
-          <img src="./assets/NatWest@2x.png" alt="Halifax@2x" height="110px" />
-        </div>
-        <div className="hover-text-left">
+    if(this.state.data[0]){
+      return (
+        <div className="hover-card-main">
           <div>
-            <div className="head">Banks</div>
-            <div>{creditdata.banks[0].bankName}</div>
+            <img src="./assets/NatWest@2x.png" alt="Halifax@2x" height="110px" />
           </div>
-          <div>
-            <div className="head">Credit limit</div>
-            <div> &#8356;{creditdata.banks[0].accounts[0].creditLimit}</div>
-          </div>
-          <div>
-            <div className="head">Available Credit</div>
-            <div> &#8356;{creditdata.banks[0].accounts[0].availableCredit}</div>
-          </div>
-        </div>
-        <div className="hover-text-right">
-          <div>
-            <div className="head">Min Due Balance</div>
-            <div>&#8356;{creditdata.banks[0].accounts[0].minBalanceDue}</div>
-          </div>
-          <div>
-            <div className="head">Due Date</div>
+          <div className="hover-text-left">
             <div>
-                {new Date(eval(creditdata.banks[0].accounts[0].dueDate)).toDateString()}
+              <div className="head">Banks</div>
+              <div>{this.state.data[0].banks[0].bankName}</div>
+            </div>
+            <div>
+              <div className="head">Credit limit</div>
+              <div> &#8356;{this.state.data[0].banks[0].accounts[0].creditLimit}</div>
+            </div>
+            <div>
+              <div className="head">Available Credit</div>
+              <div> &#8356;{this.state.data[0].banks[0].accounts[0].availableCredit}</div>
+            </div>
+          </div>
+          <div className="hover-text-right">
+            <div>
+              <div className="head">Min Due Balance</div>
+              <div>&#8356;{this.state.data[0].banks[0].accounts[0].minBalanceDue}</div>
+            </div>
+            <div>
+              <div className="head">Due Date</div>
+              <div>
+                  {new Date(eval(this.state.data[0].banks[0].accounts[0].dueDate)).toDateString()}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
+    else{
+      return (<h1>Please wait loading...</h1>)
+    }
   }
 }
 
