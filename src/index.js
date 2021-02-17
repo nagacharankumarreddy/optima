4cf43eff956ec11877e5b64fdda75c805637d879
nagacharankumarreddy <nagacharankumarreddy.com>
2021-02-17 12:09:47 +0530

    Homepage styles written in separate file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import "./homepage.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import reportWebVitals from "./reportWebVitals";
 
